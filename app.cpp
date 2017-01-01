291421eae7d66960e76dbc7abf703db9a057d501
williamblair <bblair12876@aol.com>
2017-01-01 11:13:23 -0500

    Update app.cpp

--- app.cpp
+++ app.cpp
@@ -19,32 +19,12 @@ void PSX_App::run(void)
     SetDumpFnt(FntOpen(5, 20, 320, 240, 0, 512)); //x,y, max length x,y,
                                                  // auto bg clear, max chars
 
-    //POLY_F3 tri; // a simple triangle primitive
-    //SetPolyF3(&tri); // register the triangle
-
-	//setXY3(&tri, 50, 100, 100, 50, 150, 100); // set 2d coords
-	//setRGB0(&tri, 255, 0, 0); // make the triangle red
-
-
     while(1)
     {
 
-        //FntPrint("Hello World!\n\nBJ IS HERE!\n");
+        //FntPrint("Hello World!\n");
 
-        //DrawPrim(&tri);
-        //DrawPrim(&line);
-
-        /* draw a bunch of rectangles */
-        for(int i=0; i<50; i++){
-            u_int tlX = rand()%(screen_width-100);
-            u_int tlY = rand()%(screen_width-100);
-            u_int brX = tlX + rand()%200;
-            u_int brY = tlY + rand()%200;
-            drawRect(tlX, tlY,
-                     brX, brY,
-                     rand()%255, rand()%255, rand()%255);
-        }
-
+	/* DRAWING GOES HERE! */
 
         drawFrame(); // draw the frame
     }
